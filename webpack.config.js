ce51f702d4073dbd068b83af8082be4c57cbd8af
wuyihao <840461198@qq.com>
2019-06-21 16:30:00 +0800

    [move]

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ let alias = {
   MODEL: resolveSrc('models'),
   PAGE: resolveSrc('pages'),
   SERVICE: resolveSrc('services'),
-  UTIL: resolveSrc('common/utils'),
+  UTIL: resolveSrc('common/util'),
   CONST: resolveSrc('constants'),
 };
 
